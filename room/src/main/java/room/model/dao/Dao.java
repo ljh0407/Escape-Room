e0ac7b30f4a7845e424b5cc9ad0eecc70636e16e
JunJungyu <14dbwjddl@naver.com>
2022-10-26 21:47:33 +0900

    css&빌드패치

--- room/src/main/java/room/model/dao/Dao.java
+++ room/src/main/java/room/model/dao/Dao.java
@@ -1,5 +1,23 @@
 package room.model.dao;
 
-public class Dao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class Dao {
+	Connection con;
+	PreparedStatement ps;
+	ResultSet rs;
+	
+   public Dao() {
+      try {
+      Class.forName("com.mysql.cj.jdbc.Driver");  
+            con=DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/room", 
+            "root", 
+            "1234");
+            System.out.println("DB연동 성공");
+   }catch (Exception e) {System.out.println("DB연동 실패");}
+}
 }
