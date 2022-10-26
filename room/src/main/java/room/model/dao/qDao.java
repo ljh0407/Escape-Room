ec5813d0d6c5ad12dbccf56b4d827aec89d495ff
JunJungyu <14dbwjddl@naver.com>
2022-10-26 21:50:23 +0900

    유저/문의사항

---
+++ room/src/main/java/room/model/dao/qDao.java
@@ -0,0 +1,42 @@
+package room.model.dao;
+
+import java.util.ArrayList;
+
+import room.model.dto.QDTO;
+
+public class qDao extends Dao{
+	private static qDao qdao = new qDao();
+	public static qDao getIncetance() {return qdao;}
+	
+	//1.글쓰기 고은시[10/26] 글 등록파일추가 다오수정
+	public boolean write(String btitle , String bcontent ,String  bfile , int mno) {
+		String sql = "insert into Questions (btitle,bcontent,bfile,mno) values(?,?,?,?)";
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setString(1, btitle);
+			ps.setString(2, bcontent);
+			ps.setString(3, bfile);
+			ps.setInt(4, mno);
+			ps.executeUpdate();
+			return true;
+		} catch (Exception e) {System.out.println("글등록"+e);}
+		return false;
+	}
+	//2.문의사항list 고은시 [10/26] 완료
+	public ArrayList<QDTO> getlist(){
+		ArrayList<QDTO> list = new ArrayList<>();
+		String sql ="select * from Questions";
+		try {
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				QDTO dto = new QDTO(
+						rs.getInt(1), rs.getString(2),rs.getString(3),rs.getString(4),
+						rs.getString(5),rs.getString(6),rs.getInt(7));
+				list.add(dto);
+			}
+			return list;
+		} catch (Exception e) {System.out.println("출력오류"+e);}
+			return null;
+	}
+}
---
+++ room/src/main/java/room/model/dto/USERDTO.java
@@ -0,0 +1,56 @@
+package room.model.dto;
+
+public class USERDTO {
+	//필드
+		private int mno;
+		private String mname;
+		private String mid;
+		private String mpw;
+		private String mphone;
+		public USERDTO() {
+			super();
+		}
+		public USERDTO(int mno, String mname, String mid, String mpw, String mphone) {
+			super();
+			this.mno = mno;
+			this.mname = mname;
+			this.mid = mid;
+			this.mpw = mpw;
+			this.mphone = mphone;
+		}
+		public int getMno() {
+			return mno;
+		}
+		public void setMno(int mno) {
+			this.mno = mno;
+		}
+		public String getMname() {
+			return mname;
+		}
+		public void setMname(String mname) {
+			this.mname = mname;
+		}
+		public String getMid() {
+			return mid;
+		}
+		public void setMid(String mid) {
+			this.mid = mid;
+		}
+		public String getMpw() {
+			return mpw;
+		}
+		public void setMpw(String mpw) {
+			this.mpw = mpw;
+		}
+		public String getMphone() {
+			return mphone;
+		}
+		public void setMphone(String mphone) {
+			this.mphone = mphone;
+		}
+		@Override
+		public String toString() {
+			return "misroomDTO [mno=" + mno + ", mname=" + mname + ", mid=" + mid + ", mpw=" + mpw + ", mphone=" + mphone
+					+ "]";
+		}
+}
